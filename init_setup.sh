2ab93a18f75a5a1d982fe0204b33a12faa6adb85
Ritika-02 <ritikabhandari049@gmail.com>
2023-10-31 21:38:19 +0530

    updates

--- init_setup.sh
+++ init_setup.sh
@@ -3,15 +3,15 @@
 echo [$(date)]: "START"
 
 
-echo [$(date)]: "creating env with python 3.8 version" 
+echo [$(date)]: "creating env with python 3.9 version" 
 
 
-conda create --prefix ./env python=3.8 -y
+conda create --prefix ./project_env python=3.9 -y
 
 
 echo [$(date)]: "activating the environment" 
 
-source activate ./env
+conda activate ./project_env
 
 echo [$(date)]: "installing the development requirements" 
 
