2e9a7ad37d10c7d29ad83cf3feba805c714e817d
Ritika-02 <ritikabhandari049@gmail.com>
2023-11-02 17:36:19 +0530

    Logger File updates

---
+++ src/DimondPricePrediction/logger.py
@@ -0,0 +1,29 @@
+import logging
+import os
+from datetime import datetime
+
+
+# creating a log file
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Creating a Log Folder
+log_path = os.path.join(os.getcwd(),"logs")
+
+#created a directory, where above files & folder will be there
+os.makedirs(log_path, exist_ok=True)
+
+
+#Entire log file path
+LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+
+
+#creating logging configuration
+
+logging.basicConfig(level=logging.INFO,
+                    filename= LOG_FILE_PATH,
+                    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s" 
+
+                    )
+
+
+
